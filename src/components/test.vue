7d1d19b5d77329785cf49fc1e7bc86e346bc064f
OttaviaFeletig <ottafel@hotmail.it>
2018-10-31 18:03:51 +0100

    creating some components

---
+++ src/components/test.vue
@@ -0,0 +1,14 @@
+<template>
+    <h3>I am {{myName}} and I am {{myAge}} years old</h3>
+
+    <p v-for="(num, index) num in number" :key="index">{{ num }}</p>
+
+</template>
+
+<script>
+export default {
+    name: "test",
+    props: ['myName', 'myAge', 'number']
+}
+</script>
+
--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+//one way to import the path
 import Home from './views/Home.vue'
 
 Vue.use(Router)
@@ -19,7 +20,13 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
+      //this is the second way to import the path
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import(/* webpackChunkName: "contact" */ './views/Contact.vue')
     }
   ]
 })
